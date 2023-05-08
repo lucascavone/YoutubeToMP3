469fe78378943d6b69b667396ea278f96d227adc
lucascavone <lbscavone@gmail.com>
2023-05-08 14:19:34 -0400

    Modified test.py

--- test.py
+++ test.py
@@ -1,20 +1,14 @@
-from mutagen.id3 import ID3NoHeaderError
-from mutagen.id3 import ID3, TIT2, TALB, TPE1, TPE2, COMM, TCOM, TCON, TDRC, TRCK
+import yt_dlp
 
-fname = "/Users/luca/Desktop/Media/Music/NewMusic_04-2023/song1test2.mp3"
-# Read the ID3 tag or create one if not present
-try:
-    tags = ID3(fname)
-except ID3NoHeaderError:
-    print("Adding ID3 header")
-    tags = ID3(filename=fname)
-    tags.delete()  # delete existing ID3 tag if present
-    tags.save(v2_version=3)  # set ID3 tag version to 2.3
+ydl = yt_dlp.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
 
-tags["TIT2"] = TIT2(encoding=3, text=u"Daydreamin'")
-tags["TALB"] = TALB(encoding=3, text=u'Yours Truly')
-tags["TPE1"] = TPE1(encoding=3, text=u'Ariana Grande')
-tags["TCON"] = TCON(encoding=3, text=u'R&B/Soul, Pop')
-tags["TDRC"] = TDRC(encoding=3, text=u'2013')
+video_url = str(input('Enter URL: \n >> '))
+info = ydl.extract_info(video_url, download=False)
 
-tags.save(fname)
+title = info['title']
+uploader = info['uploader']
+description = info['description']
+
+print(f'\nTitle: {title}')
+print(f'Uploader: {uploader}')
+print(f'Description: {description}')
